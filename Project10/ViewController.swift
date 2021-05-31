d79f68ad00f96936078b7457aa3bff08903579ae
Azat <kaumov.a.r@gmail.com>
2021-05-31 21:17:59 +0300

    Add image picker

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -7,10 +7,49 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+        
+        guard let jpegData = image.jpegData(compressionQuality: 0.8) else {
+            return
+        }
+        
+        let fileName = UUID().uuidString
+        let destinationPath = getDocumentsDirectory().appendingPathComponent(fileName)
+        
+        try? jpegData.write(to: destinationPath)
+        
+        dismiss(animated: true)
+    }
+    
+    @objc func addButtonTapped() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true)
+    }
+
+    private func initNavBar() {
+        navigationItem.leftBarButtonItem = .init(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonTapped)
+        )
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initNavBar()
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
